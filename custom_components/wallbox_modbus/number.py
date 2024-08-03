878c2ba2dc8749707fc96be4da546c966e5e2d85
Ronald Pijnacker <ronald.pijnacker@gmail.com>
2024-08-03 13:38:45 +0200

    Delay creating NumberEntityDescriptions

--- custom_components/wallbox_modbus/number.py
+++ custom_components/wallbox_modbus/number.py
@@ -13,45 +13,44 @@ from homeassistant.const import (
 from .const import DOMAIN
 from .entity import WallboxModbusEntity
 
-CurrentSetpointEntityDescription = NumberEntityDescription(
-    key="current_setpoint",
-    name="Current setpoint",
-    native_unit_of_measurement=UnitOfElectricCurrent.AMPERE,
-    device_class=NumberDeviceClass.CURRENT,
-    native_min_value=6,
-    native_max_value=32,
-    native_step=1,
-    mode=NumberMode.SLIDER,
-)
-PowerSetpointEntityDescription = NumberEntityDescription(
-    key="power_setpoint",
-    name="Power setpoint",
-    native_unit_of_measurement=UnitOfPower.WATT,
-    device_class=NumberDeviceClass.POWER,
-    native_min_value=1380,
-    native_max_value=7400,
-    native_step=10,
-    mode=NumberMode.BOX,
-)
-
 
 async def async_setup_entry(hass, entry, async_add_devices):
     """Set up the sensor platform."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
+
     max_current = coordinator.data.get('max_available_current', 32)
-    CurrentSetpointEntityDescription.native_max_value = max_current
+    entity_description = NumberEntityDescription(
+        key="current_setpoint",
+        name="Current setpoint",
+        native_unit_of_measurement=UnitOfElectricCurrent.AMPERE,
+        device_class=NumberDeviceClass.CURRENT,
+        native_min_value=6,
+        native_max_value=max_current,
+        native_step=1,
+        mode=NumberMode.SLIDER,
+    )
+    current_entity = WallboxModbusCurrentSetpoint(
+        coordinator=coordinator,
+        entity_description=entity_description,
+    )
+
     max_power = coordinator.data.get('max_available_power', 7400)
-    PowerSetpointEntityDescription.native_max_value = max_power
-    async_add_devices([
-        WallboxModbusCurrentSetpoint(
-            coordinator=coordinator,
-            entity_description=CurrentSetpointEntityDescription,
-        ),
-        WallboxModbusPowerSetpoint(
-            coordinator=coordinator,
-            entity_description=PowerSetpointEntityDescription,
-        ),
-    ])
+    entity_description = NumberEntityDescription(
+        key="power_setpoint",
+        name="Power setpoint",
+        native_unit_of_measurement=UnitOfPower.WATT,
+        device_class=NumberDeviceClass.POWER,
+        native_min_value=1380,
+        native_max_value=max_power,
+        native_step=10,
+        mode=NumberMode.BOX,
+    )
+    power_entity = WallboxModbusPowerSetpoint(
+        coordinator=coordinator,
+        entity_description=entity_description,
+    )
+
+    async_add_devices([current_entity, power_entity])
 
 
 class WallboxModbusNumber(WallboxModbusEntity, NumberEntity):
